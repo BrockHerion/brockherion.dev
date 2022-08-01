9ea5ed654c4badd4f5def4a3297754665912283f
Brock Herion <brockheriondev@gmail.com>
2022-08-01 08:42:08 -0500

    Astro update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), react(), tailwind()],
-	server: {
-		tailwindConfig: './tailwind.config.cjs'
-	}
+  integrations: [preact(), react(), tailwind(), mdx()],
+  server: {
+    tailwindConfig: './tailwind.config.cjs'
+  }
 });
