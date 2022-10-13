d85c1890fc65b20d1d3664621d49dfb005ccf14f
Brock Herion <brockheriondev@gmail.com>
2022-10-13 06:47:58 -0500

    Completed article

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,17 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		ssr: {
+			external: ['svgo']
+		}
+	},
   integrations: [preact(), react(), tailwind({    
 		config: { applyBaseStyles: false },
 	}), mdx()],
+	markdown: {
+		shikiConfig: {
+			theme: 'dark-plus'
+		}
+	}
 });
