b96daa6f10e2de7af005e2669da54824d7f17585
Brock Herion <brockheriondev@gmail.com>
2023-03-07 05:44:23 -0600

    Added sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,26 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		ssr: {
-			external: ['svgo']
-		}
-	},
-  integrations: [preact(), react(), tailwind({    
-		config: { applyBaseStyles: false },
-	}), mdx()],
-	markdown: {
-		shikiConfig: {
-			theme: 'dark-plus'
-		}
-	}
+  vite: {
+    ssr: {
+      external: ['svgo']
+    }
+  },
+  integrations: [preact(), react(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx(), sitemap()],
+	site: "https://brockherion.dev",
+  markdown: {
+    shikiConfig: {
+      theme: 'dark-plus'
+    }
+  }
 });
